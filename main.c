d0c72f41bbe157b04c81a1f3243032b1e40ce439
Karthika <karthika.thulaseedharan@trenser.com>
2026-02-25 10:55:31 +0530

    Uploaded modified code from different folder

--- main.c
+++ main.c
@@ -14,30 +14,30 @@
 //******************************* Include Files ********************************
 #include "stringManipulation.h"
 
-//******************************* Local Types ********************************* 
- 
-//***************************** Local Constants ******************************* 
- 
-//***************************** Local Variables ******************************* 
- 
-//****************************** Local Functions ******************************
+//******************************* Local Types ********************************** 
+
+//***************************** Local Constants ******************************** 
+
+//***************************** Local Variables ******************************** 
+
+//****************************** Local Functions *******************************
 
 //****************************** FUNCTION_HEADER ******************************* 
 //Purpose : It calls the function that implements the string manipulation logic
 //Inputs  : None
-//Outputs : Calls user intertface function for performing string manipulation
+//Outputs : Calls user intertface function to perform string manipulation
 //Return  : returns 0 
 //Notes   : None
 //****************************************************************************** 
 int main()
-{
-	
+{	
 	if(!strManipUserInterface())
 	{
 		printf("Program failed\n");
 		return 0;
 	}
-
 	return 0;
 }
+
+
 // EOF
--- stringManipulation/stringManipulation.c
+++ stringManipulation/stringManipulation.c
@@ -15,162 +15,180 @@
 #include "stringManipulation.h"
 
 //******************************* Local Types **********************************
+//enum for various functionality
 typedef enum{
 	OPTION_UPPER_TO_LOWER = 1,
 	OPTION_LOWER_TO_UPPER,
 	OPTION_CONCAT,
 	OPTION_EXIT
 }OPTION_TYPE;
+//Function pointer for string manipulation functions
+typedef bool (*strManipFuncHandler)(uint8_t* );
+//structure having user entry, corresponding menu label and function pointer
+typedef struct {
+    OPTION_TYPE eInpOption;
+    uint8_t* ucMenuString;
+    strManipFuncHandler pHandler;
+}strManipTask;
 
 //***************************** Local Constants ********************************
-#define STRMAIP_MAX_STR_SIZE     (100)
 #define STRMAIP_MIN_UPPER_CASE   ('A')
 #define STRMAIP_MAX_UPPER_CASE   ('Z')
 #define STRMAIP_MIN_LOW_CASE     ('a')
 #define STRMAIP_MAX_LOW_CASE     ('z')
 #define STRMAIP_UPPER_LOWER_DIFF (32)
+#define STRMAIP_MAX_STR_SIZE     (100)
 
-static OPTION_TYPE unInpOption = 0;
-static uint16_t unStrIndex = 0;
-static uint8_t pucInpStr[STRMAIP_MAX_STR_SIZE] = {0};
+static strManipTask strManiptasks[] = {
+    {OPTION_UPPER_TO_LOWER, "Uppercase-Lowercase",
+        (strManipFuncHandler)strManipUpperLower},
+    {OPTION_LOWER_TO_UPPER, "Lowercase-Uppercase",
+        (strManipFuncHandler)strManipLowerUpper},
+    {OPTION_CONCAT, "String concatenate",
+		(strManipFuncHandler)strManipConcatenate}
+};
+#define TASK_COUNT (sizeof(strManiptasks)/(sizeof(strManiptasks[0])))
+
+//***************************** Local Variables ********************************
 static uint8_t pucCatStr[STRMAIP_MAX_STR_SIZE] = {0};
+static uint8_t pucInpStr[STRMAIP_MAX_STR_SIZE] = {0};
+static uint16_t unStrIndex = 0;
 static uint16_t unCatStrIndex = 0;
+static uint8_t ucIterIndex = 0;
+static uint8_t unInpNum = 0;
+static bool blReturnFlag = false;
 
 //****************************** Local Functions *******************************
-static bool strManipUpperLower(char *);
-static bool strManipLowerUpper(char *);
-static bool strManipConcatenate(char *, char *);
-static bool strManipDisplay(char *);
+static bool strManipDisplay(uint8_t* );
+static bool strManipUpperLower(uint8_t* );
+static bool strManipLowerUpper(uint8_t* );
+static bool strManipConcatenate(uint8_t*);
 
 //****************************** FUNCTION_HEADER ******************************* 
 //Purpose : This function takes the option for particular string function, stri-
 //          ng input and calls the respective function 
 //Inputs  : None
 //Outputs : corresponding function is called for string manipulation 
-//Return  : returns true on succesfull execution
+//Return  : blReturnFlag - bool value based on execution
 //Notes   : None
 //****************************************************************************** 
 bool strManipUserInterface(void)
 {
     do
     {
-        printf("Enter the operation to be performed\n");
-        printf("1. Uppercase-Lowercase\n2. Lowercase-Uppercase\n");
-        printf("3. Concatenate\n4. Exit\n");
-        scanf("%d",&unInpOption);
-        switch(unInpOption)
-	    {
-		    case OPTION_UPPER_TO_LOWER:
-                printf("Enter the string\n");
-                scanf(" %[^\n]",pucInpStr);
-			    strManipUpperLower(pucInpStr);
-			    break;
-		    case OPTION_LOWER_TO_UPPER:
-                printf("Enter the string\n");
-                scanf(" %[^\n]",pucInpStr);
-			    strManipLowerUpper(pucInpStr);
-			    break;
-		    case OPTION_CONCAT:
-                printf("Enter the strings to be concatenated:\n");
-                scanf(" %[^\n]",pucInpStr);
-	            scanf(" %[^\n]",pucCatStr);
-			    strManipConcatenate(pucInpStr,pucCatStr);
-                break;
-            case OPTION_EXIT:
-                printf("Program exits\n");
-                break;
-		    default:
-			    printf("Invalid input\n");
-			    break;
-	    }
+        printf("String Operatrions\n"); 
 
+        for(ucIterIndex = 0; ucIterIndex < TASK_COUNT; ucIterIndex++)
+        {
+            printf("%d. %s\n",strManiptasks[ucIterIndex].eInpOption, 
+                strManiptasks[ucIterIndex].ucMenuString);
+        }
+        printf("%d. Exit\n",OPTION_EXIT);
+        scanf("%hhd",&unInpNum);
+
+        for(ucIterIndex = 0; ucIterIndex < TASK_COUNT; ucIterIndex++)
+        {
+            if(strManiptasks[ucIterIndex].eInpOption == unInpNum)
+            {
+                printf("Enter the string\n");
+                scanf(" %[^\n]",pucInpStr);
+                strManiptasks[ucIterIndex].pHandler(pucInpStr);
+            }
+        }
     }
-    while(unInpOption != OPTION_EXIT);
-    if (OPTION_EXIT == unInpOption) {
-        return true; 
+    while(OPTION_EXIT != unInpNum);
+
+    if (OPTION_EXIT == unInpNum) {
+        blReturnFlag = true; 
     } else{
-        return false;
+        blReturnFlag = false;
     }
+    return blReturnFlag;
 }
 
 //****************************** FUNCTION_HEADER ******************************* 
 //Purpose : This function converts upperccase characters in the string to lower-
 //          case
-//Inputs  : User input string 
+//Inputs  : pucinpStr - User input string
 //Outputs : user input string is converted to lowercase 
-//Return  : returns true on succesfull execution
+//Return  : blReturnFlag - bool value based on execution
 //Notes   : checks if the char in the string is uppercase then it is added to 
 //          get the corresponding lowercase char
 //******************************************************************************
-static bool strManipUpperLower(char *pucInpStr)
+static bool strManipUpperLower(uint8_t* pucInpStr)
 {   
     if (NULL != pucInpStr) {
         unStrIndex = 0;
+
         while('\0' !=  pucInpStr[unStrIndex])
         {
-		    if(pucInpStr[unStrIndex] >= STRMAIP_MIN_UPPER_CASE && 
-                pucInpStr[unStrIndex] <= STRMAIP_MAX_UPPER_CASE)
+		    if((STRMAIP_MIN_UPPER_CASE <= pucInpStr[unStrIndex]) && 
+                (STRMAIP_MAX_UPPER_CASE >= pucInpStr[unStrIndex]))
 		    {
 			    pucInpStr[unStrIndex] += STRMAIP_UPPER_LOWER_DIFF;
 		    }
-		unStrIndex++;
+		    unStrIndex++;
 	    }
 	    strManipDisplay(pucInpStr); 
-        return true;
-        
+        blReturnFlag = true;        
     }
     else
     {
-        return false;
+        blReturnFlag = false;
     }
-    
+    return blReturnFlag;   
 }
 
 //****************************** FUNCTION_HEADER ******************************* 
 //Purpose : This function converts lowercase characters in the string to upper-
 //          case
-//Inputs  : user input string
+//Inputs  : pucinpStr - User input string
 //Outputs : user input string is converted to uppercase
-//Return  : returns true
+//Return  : blReturnFlag - bool value based on execution
 //Notes   : checks if the char in the string is lowercase then it is subtracted
 //          to get the corresponding uppercase char
 //******************************************************************************
-static bool strManipLowerUpper(char *pucInpStr)
+static bool strManipLowerUpper(uint8_t* pucInpStr)
 {
     if (NULL != pucInpStr) {
         unStrIndex = 0;
+
 	    while('\0' != pucInpStr[unStrIndex])
 	    {
-		    if(pucInpStr[unStrIndex] >= STRMAIP_MIN_LOW_CASE && 
-                pucInpStr[unStrIndex] <= STRMAIP_MAX_LOW_CASE)
+		    if(STRMAIP_MIN_LOW_CASE <= pucInpStr[unStrIndex] && 
+                STRMAIP_MAX_LOW_CASE >= pucInpStr[unStrIndex])
 		    {
 			    pucInpStr[unStrIndex] -= STRMAIP_UPPER_LOWER_DIFF;
 		    }
 		    unStrIndex++;
 	    }
 	    strManipDisplay(pucInpStr);
-        return true; 
-        
+        blReturnFlag = true;         
     }
     else
-        return false;
-      
+        blReturnFlag = false;
+    return blReturnFlag;     
 }
 
 //****************************** FUNCTION_HEADER ******************************* 
 //Purpose : The function concatanates two strings that is inputted by the user
-//Inputs  : User input strings
+//Inputs  : pucinpStr - User input string
 //Outputs : user input strings are concatenated
-//Return  : returns true 
+//Return  : blReturnFlag - bool value based on execution
 //Notes   : Finds the end of the first and the second string added from the last
 //          index for the first string
 //******************************************************************************
-static bool strManipConcatenate(char *pucInpStr, char *pucCatStr)
+static bool strManipConcatenate(uint8_t* pucInpStr)
 {
+    printf("Enter the string to be concatenated\n");
+    scanf(" %[^\n]",pucCatStr);
+
     if (NULL != pucInpStr) {
         unCatStrIndex = 0, unStrIndex = 0;
+
 	    while('\0' != pucInpStr[unStrIndex])
 		    unStrIndex++;
+
 	    while('\0' != pucCatStr[unCatStrIndex])
 	    {
 		    pucInpStr[unStrIndex] = pucCatStr[unCatStrIndex];
@@ -179,29 +197,33 @@ static bool strManipConcatenate(char *pucInpStr, char *pucCatStr)
 	    }
 	    pucInpStr[unStrIndex] = '\0';
         strManipDisplay(pucInpStr);
-        return true;
+        blReturnFlag = true;
     }
     else
-        return false;
-	
+    {
+        blReturnFlag = false;
+    }       
+	return blReturnFlag;
 }
 
 //****************************** FUNCTION_HEADER ******************************* 
 //Purpose : The function is used to display the string that is passed to the fu-
 //          nction
-//Inputs  : Input string to be displayed
+//Inputs  : pucinpStr - User input string
 //Outputs : string get printed in the console
-//Return  : returns on displaying
+//Return  : blReturnFlag - bool value based on execution
 //Notes   : Use puts library function to display
 //******************************************************************************
-static bool strManipDisplay(char *pucInpStr)
+static bool strManipDisplay(uint8_t* pucInpStr)
 {
     if (NULL != pucInpStr) {
         puts(pucInpStr);
-        return true;
+        blReturnFlag = true;
     }
     else
-        return false
-    
+        blReturnFlag = false;
+    return blReturnFlag;    
 }
+
+
 // EOF
